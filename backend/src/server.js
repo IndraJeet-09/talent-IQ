40a1f49b4003cbca412de7d48f22af2144eda2bc
Indrajeet Chouhan <indrajeetchouhan680@gmail.com>
2025-12-27 12:53:08 +0530

    improving server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,11 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
 import { ENV } from "./lib/env.js";
 
 console.log("ENV loaded:", ENV); // 🔍 debug log
 
 const app = express();
 
-// ✅ Proper __dirname for ES modules
-// const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.resolve();
 
 // Health check
@@ -19,8 +16,6 @@ app.get("/health", (req, res) => {
 // Serve frontend in production
 if (ENV.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "../frontend/dist");
-
-//   console.log("Serving frontend from:", frontendPath);
 
   app.use(express.static(frontendPath));
 
