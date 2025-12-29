b651e710b0f92b8b2f934172923a8a1df1293779
Indrajeet Chouhan <indrajeetchouhan680@gmail.com>
2025-12-29 22:20:04 +0530

    server side auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import {serve} from "inngest/express"
 import {inngest ,functions} from './lib/inngest.js'
+import { clerkMiddleware } from '@clerk/express';
+import chatRoutes from './routes/chatRoutes.js'
 
 console.log("ENV loaded:", ENV); // 🔍 debug log
 
@@ -13,15 +15,20 @@ const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json());
+
+app.use(clerkMiddleware());
 // credentials: true meaning? => server allows browser to include cookies on request
 app.use(cors({origin:ENV.CLIENT_URL, credentials: true }));
 
 app.use("/api/inngest", serve({client: inngest ,functions}))
+app.use("/api/chat", chatRoutes)
 
 // Health check
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "API is up and running" });
 });
+
+
 
 // Serve frontend in production
 if (ENV.NODE_ENV === "production") {
