c1650c8e4461d3b8979ccd0356b5e686e82ce2aa
Indrajeet Chouhan <indrajeetchouhan680@gmail.com>
2025-12-30 21:53:18 +0530

    Landing page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,19 @@ import { Navigate, Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import ProblemPages from './pages/ProblemPages'
 import {Toaster} from 'react-hot-toast';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
-  const {isSignedIn} =  useUser();
+  const {isSignedIn, isLoaded} =  useUser();
+
+  if(!isLoaded) return null;
   return (
     <>
     <Toaster toastOptions={{duration:3000}} />
     <Routes>
-      
-      
-      <Route path='/' element={<HomePage />}/>
+
+      <Route path='/' element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} /> }/>
+      <Route path='/dashboard' element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} /> }/>
       
       <Route path='/problems' element={isSignedIn ? <ProblemPages /> : <Navigate to={"/"} />}/>
     </Routes>
@@ -24,6 +27,3 @@ function App() {
 }
 
 export default App
-
-// tw, daisyui, react-router, react-hot-toaster, 
-// react-query aka tanstack query, axios
