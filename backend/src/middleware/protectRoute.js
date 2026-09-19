5169dbd2072787c2f36751fc0b0f0fa99d258996
IndraJeet09 <indrajeetchouhan680@gmail.com>
2026-09-20 04:35:09 +0530

    git

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -2,11 +2,11 @@ import { requireAuth } from "@clerk/express";
 import User from "../models/User.js";
 
 export const protectRoute = [
-    requireAuth(),
+    requireAuth({ signInUrl: false }),
     async(req, res, next) => {
         try {
             const clerkId = req.auth().userId;
-            if(!clerkId) return res.status(404).json({msg : "Unauthorized - invalid token"});
+            if(!clerkId) return res.status(401).json({msg : "Unauthorized"});
 
             const user = await User.findOne({clerkId});
             if(!user) return res.status(404).json({msg : "User not found"});
